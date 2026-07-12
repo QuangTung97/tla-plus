d246818cd916f12f2deb2da1d610ba08b4bd416d
quangtung97 <quangtung29121997@gmail.com>
2026-07-12 08:28:17 +0700

    Add simulation check

---
+++ MultiPaxosV2/simulate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+java \
+    -Xmx12g \
+    -cp /home/quangtung/.vscode/extensions/tlaplus.vscode-ide-2026.6.242335/tools/tla2tools.jar \
+    -XX:+UseParallelGC \
+    -DTLA-Library= tlc2.TLC MultiPaxosV2.tla \
+    -tool -simulate num=1000 -seed 123456789 \
+    -config /home/quangtung/tla-plus/MultiPaxosV2/MultiPaxosV2.cfg \
+    -workers 2
